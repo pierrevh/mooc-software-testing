8dd5488a38179f73482bb5e3ecf52819c31ba311
Pierre van Hees <pvhees@xs4all.nl>
2018-11-10 19:03:30 +0100

    Add test for a single g and fix bug

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,13 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class GHappyTest {
+    @Test
+    void oneGeeIsUnhappy() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("g"));
+    }
 }
