d64b6c86ed1102719aeeb82bd0d55e5b0ed5d622
Pierre van Hees <pvhees@xs4all.nl>
2018-11-11 13:58:10 +0100

    Add tests for happy g's and all variations of unhappy ones

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,13 +1,48 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GHappyTest {
+    private GHappy gHappy;
+    @BeforeEach
+    void setUp() {
+        gHappy = new GHappy();
+    }
+
+    @Test
+    void emptyInputIsHappy() {
+        assertTrue(gHappy.gHappy(""));
+    }
+
     @Test
     void oneGeeIsUnhappy() {
-        GHappy gHappy = new GHappy();
         assertFalse(gHappy.gHappy("g"));
+    }
+
+    @Test
+    void twoGeesAreHappy() {
+        assertTrue(gHappy.gHappy("gg"));
+    }
+
+    @Test
+    void twoSeparatedGeesAreUnhappy() {
+        assertFalse(gHappy.gHappy("gxg"));
+        assertFalse(gHappy.gHappy("gxcg"));
+        assertFalse(gHappy.gHappy("xgxg"));
+        assertFalse(gHappy.gHappy("xgxgx"));
+        assertFalse(gHappy.gHappy("dssadgxasdsdagasdasdas"));
+    }
+
+    @Test
+    void anySeparatedGeesAreUnhappy() {
+        assertFalse(gHappy.gHappy("gxgxg"));
+        assertFalse(gHappy.gHappy("gxcgxg"));
+        assertFalse(gHappy.gHappy("xgxgsg"));
+        assertFalse(gHappy.gHappy("xgxgx"));
+        assertFalse(gHappy.gHappy("dssgadgxasdgsdgagasdgasdgags"));
     }
 }
